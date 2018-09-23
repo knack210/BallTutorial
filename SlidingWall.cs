ae0422e78734f180b009b9cfe1444c6a4d196e14
knack210 <nickderosa@knights.ucf.edu>
2018-09-23 15:42:25 -0400

    Add files via upload
    Allows objects to slide back and forth

---
+++ SlidingWall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Also used with sliding penalty
+public class SlidingWall : MonoBehaviour {
+
+    public float speed;
+    public int direction;
+    private float start;
+
+	void Start ()
+    {
+        start = transform.position.x;
+	}
+	
+	void Update ()
+    {
+        // Found this function after doing a bit of research online
+        transform.position = new Vector3(start + Mathf.PingPong(Time.time * speed, direction), transform.position.y, transform.position.z);
+	}
+}
